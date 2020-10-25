b11ef90bbd76476075893d44d8b55915dfeaae03
22011283 <22011283@student.bucks.ac.uk>
2020-10-25 17:28:31 +0000

    Move course printing to Course class

--- app03-studentGrades/Course.java
+++ app03-studentGrades/Course.java
@@ -12,7 +12,10 @@ public class Course {
     private String title;
     // Course modules
     private ArrayList<Module> modules = new ArrayList();
-    
+
+    // Total module credits
+    private int credits = 0;
+
     // Credits each module is worth
     public static int MODULE_CREDITS = 15;
 
@@ -49,11 +52,56 @@ public class Course {
      */
     @Override
     public String toString() {
-        String ret = this.title += "\n";
-        for (Module module : this.modules) {
-            ret += "  " + module + "\n";
+        String ret = this.title + "\n";
+        
+        if (this.modules.size() == 0) {
+            ret += "Course has no modules." + "\n";
+            return ret;
         }
         
+        boolean hasUnmarkedModule = false;
+        
+        int totalMarks = 0;
+        for (Module module : this.modules) {
+            ret += " - " + module;
+
+            // Don't count unmarked modules
+            if (!module.isMarked()) {
+                hasUnmarkedModule = true;
+                ret += " (uncompleted)" + "\n";
+                continue;
+            }
+
+            this.credits += Course.MODULE_CREDITS;
+            totalMarks += module.getMark();
+            ret += "\n";
+        }
+        
+        // Calc average
+        totalMarks /= modules.size();
+        
+        ret += "\nTotal marks: " + totalMarks;
+
+        // Only assign final grade if all modules are complete
+        if (!hasUnmarkedModule) {
+            // Convert to letter grade
+            char finalGrade;
+            if (totalMarks >= 70) {
+                finalGrade = 'A';
+            } else if (totalMarks >= 60) {
+                finalGrade = 'B';
+            } else if (totalMarks >= 50) {
+                finalGrade = 'C';
+            } else if (totalMarks >= 40) {
+                finalGrade = 'D';
+            } else {
+                finalGrade = 'F';
+            }
+            
+            ret += "\nFinal grade: " + finalGrade;
+        }
+        
+        ret += "\n";
         return ret;
     }
 }
--- app03-studentGrades/Student.java
+++ app03-studentGrades/Student.java
@@ -12,8 +12,6 @@ public class Student {
     private String name;
     // the student ID
     private String id;
-    // the amount of credits for study taken so far
-    private int credits = 0;
 
     // Student's course
     private Course course;
@@ -55,20 +53,6 @@ public class Student {
     }
 
     /**
-     * Add some credit points to the student's accumulated credits.
-     */
-    public void addCredits(int additionalPoints) {
-        this.credits += additionalPoints;
-    }
-
-    /**
-     * Return the number of credit points this student has accumulated.
-     */
-    public int getCredits() {
-        return this.credits;
-    }
-
-    /**
      * Return the login name of this student. The login name is a combination
      * of the first four characters of the student's name and the first three
      * characters of the student's ID number.
@@ -81,41 +65,7 @@ public class Student {
      * Print the student's name and ID number to the output terminal.
      */
     public void print() {
-        ArrayList<Module> modules = this.course.getModules();
-        if (modules.isEmpty()) {
-            System.out.println("Student course contains no modules!");
-            return;
-        }
-        
-        int finalMark = 0;
-        for (Module module : modules) {
-            if (!module.isMarked()) {
-                System.out.println("Student has not completed all modules!");
-                return;
-            }
-
-            this.credits += Course.MODULE_CREDITS;
-            finalMark += module.getMark();
-        }
-        
-        // Calc average
-        finalMark /= modules.size();
-
-        // Convert to letter grade
-        char finalGrade;
-        if (finalMark >= 70) {
-            finalGrade = 'A';
-        } else if (finalMark >= 60) {
-            finalGrade = 'B';
-        } else if (finalMark >= 50) {
-            finalGrade = 'C';
-        } else if (finalMark >= 40) {
-            finalGrade = 'D';
-        } else {
-            finalGrade = 'F';
-        }
-
-        System.out.printf("%s, student ID: %s, credits: %d\n Final grade: %c\n",
-                          this.name, this.id, this.credits, finalGrade);
+        System.out.printf("%s (%s), %s\n",
+                          this.name, this.id, this.course);
     }
 }
